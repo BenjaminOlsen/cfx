b6f28d6a4833ceee5bbb4d6ef5bbad1c4bb08531
Benjamin Olsen <thelarrys@gmail.com>
2025-10-04 06:09:28 +0200

    ver 0.0.1

--- include/cfx/version.h
+++ include/cfx/version.h
@@ -4,6 +4,6 @@
 #define CFX_VERSION_H
 #define CFX_VERSION_MAJOR 0
 #define CFX_VERSION_MINOR 0
-#define CFX_VERSION_PATCH 0
-#define CFX_VERSION_STRING "0.0.0"
+#define CFX_VERSION_PATCH 1
+#define CFX_VERSION_STRING "0.0.1"
 #endif
