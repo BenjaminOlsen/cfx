f67c5f0132fe19b43a20ce37ad4df232a9515564
Benjamin Olsen <thelarrys@gmail.com>
2026-01-26 09:54:29 +0100

    separator string for cfx primes

--- utils/cfx_primes.c
+++ utils/cfx_primes.c
@@ -32,34 +32,54 @@ static int dec_digits(cfx_limb_t n) {
 }
 
 static void usage(const char* prog) {
-    fprintf(stderr, "Usage: %s <n> [col_width]\n", prog);
-    fprintf(stderr, "  List all primes up to n using sieve of Eratosthenes\n");
-    fprintf(stderr, "  col_width  optional line width for formatting output\n");
+    fprintf(stderr, "Usage: %s [options] <n>\n", prog);
+    fprintf(stderr, "  List all primes up to n using sieve of Eratosthenes\n\n");
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "  --width=N    Line width for formatting output (default: no wrap)\n");
+    fprintf(stderr, "  --sep=STR    Separator string (default: ', ')\n");
+    fprintf(stderr, "  -h, --help   Show this help\n");
 }
 
 int cfx_primes_run(int argc, char* argv[]) {
-    if (argc < 2 || (argc == 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0))) {
-        usage(argv[0]);
-        return argc < 2 ? 1 : 0;
+    int dmax = INT32_MAX;
+    const char* sep = ", ";
+    cfx_limb_t n = 0;
+    int have_n = 0;
+
+    for (int i = 1; i < argc; i++) {
+        const char* arg = argv[i];
+        if (strcmp(arg, "--help") == 0 || strcmp(arg, "-h") == 0) {
+            usage(argv[0]);
+            return 0;
+        } else if (strncmp(arg, "--width=", 8) == 0) {
+            dmax = (int)strtol(arg + 8, NULL, 10);
+        } else if (strncmp(arg, "--sep=", 6) == 0) {
+            sep = arg + 6;
+        } else {
+            n = (cfx_limb_t)strtol(arg, NULL, 10);
+            have_n = 1;
+        }
     }
 
-    int dmax = INT32_MAX;
-    if (argc == 3) dmax = (size_t)strtol(argv[2], NULL, 10);
+    if (!have_n) {
+        usage(argv[0]);
+        return 1;
+    }
 
-    cfx_limb_t n = (cfx_limb_t)strtol(argv[1], NULL, 10);
+    size_t sep_len = strlen(sep);
     cfx_vec_t primes = cfx_sieve_primes(n);
     int dcnt = 0;
     for (size_t k = 0; k < primes.size-1; ++k) {
-        int c = dec_digits(primes.data[k]) + 2; /* +2 for comma + space */
+        int c = dec_digits(primes.data[k]) + (int)sep_len;
         if (dcnt + c > dmax) {
             printf("\n");
             dcnt = c;
         } else {
             dcnt += c;
         }
-        printf("" CFX_PRIuLIMB ", ", primes.data[k]);
+        printf(CFX_PRIuLIMB "%s", primes.data[k], sep);
     }
-    printf("" CFX_PRIuLIMB "\n", primes.data[primes.size-1]);
+    printf(CFX_PRIuLIMB "\n", primes.data[primes.size-1]);
 
     printf("found %zu primes until " CFX_PRIuLIMB " (0x" CFX_PRIuLIMB ")\n", primes.size, n, n);
 
